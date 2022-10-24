50d1486fcd0bfecc21612f2bdc24de47f15361c0
mininekos <manuel2g2m@gmail.com>
2022-10-24 14:01:15 +0200

    V2 chistes mejorados

--- src/app/Modelo/servicio.ts
+++ src/app/Modelo/servicio.ts
@@ -5,9 +5,9 @@ export class Servicio {
     private bromas: Broma[];
     constructor(){
         this.bromas=  [
-            new Broma("¿Cual es el colmo de un jardinero)", "Que le dejen plantado"),
-            new Broma("¿Que hubiera pasado si la virgen maria tuviera 3 hijos?","Que serian tri-jisus"),
-            new Broma("Que le dice un ojo a otro ojo", "Estamos separados por narices"),
+            new Broma("¿Cual es el colmo de un jardinero?", "Que le dejen plantado"),
+            new Broma("¿Qué hubiera pasado si la virgen maria tuviera 3 hijos?","Que serian tri-jisus"),
+            new Broma("¿Qué le dice un ojo a otro ojo?", "Estamos separados por narices"),
         ];
     }
 
